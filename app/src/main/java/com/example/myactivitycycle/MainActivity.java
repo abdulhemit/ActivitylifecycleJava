509545f6b974e9ac7a9bc836bc566a14690daa9c
abdulhemit <93004625+abdulhemit@users.noreply.github.com>
2022-04-19 11:31:06 +0300

    lifecycle

---
+++ app/src/main/java/com/example/myactivitycycle/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.myactivitycycle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview);
+        editText = findViewById(R.id.editText);
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: ");
+        outState.putString("saved",textView.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState: ");
+        String savedString = savedInstanceState.getString("saved");
+        textView.setText(savedString);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    public void getName(View view){
+        String nameInput = editText.getText().toString();
+        textView.setText(nameInput);
+
+    }
+
+    public void secondActivity(View view){
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(intent);
+        finish();
+    }
+}
